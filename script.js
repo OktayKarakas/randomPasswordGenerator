dd5743988d94ac6ab156ff5b64fa05effd6799bc
catresium <66228319+catresium@users.noreply.github.com>
2022-08-09 18:57:29 +0300

    Random password generator created with HTML CSS and JS
    in this project i used js html css.

---
+++ script.js
@@ -0,0 +1,112 @@
+const characters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '~',
+  '`',
+  '!',
+  '@',
+  '#',
+  '$',
+  '%',
+  '^',
+  '&',
+  '*',
+  '',
+  '',
+  '_',
+  '-',
+  '+',
+  '=',
+  '{',
+  '[',
+  '}',
+  ']',
+  ',',
+  '|',
+  ':',
+  ';',
+  '<',
+  '>',
+  '.',
+  '?',
+  '/',
+]
+let passwordFirst = document.getElementById('password-box-first')
+let passwordSecond = document.getElementById('password-box-second')
+function generateRandomPassword() {
+  let randomPasswordFirst =
+    characters[Math.floor(Math.random() * characters.length)]
+  let randomPasswordSecond =
+    characters[Math.floor(Math.random() * characters.length)]
+
+  for (let i = 0; i < 14; i++) {
+    randomPasswordFirst +=
+      characters[Math.floor(Math.random() * characters.length)]
+  }
+  for (let i = 0; i < 14; i++) {
+    randomPasswordSecond +=
+      characters[Math.floor(Math.random() * characters.length)]
+  }
+  passwordFirst.textContent = randomPasswordFirst
+  passwordSecond.textContent = randomPasswordSecond
+}
